1aef6159fff4974605301bf2d7854496880dadcf
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2025-02-03 00:08:50 +0900

    4.3

---
+++ tank/tank.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 0.1
+const MOVE_SPEED = 200
+var rotation_velosity := 0.0
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		rotation_velosity = direction * SPEED
+	else:
+		rotation_velosity = move_toward(rotation_velosity, 0, SPEED)
+	
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	if direction_y:
+		velocity = Vector2.from_angle(rotation) * MOVE_SPEED * direction_y * -1
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, delta * 10)
+	
+	rotation += rotation_velosity
+
+	move_and_slide()
